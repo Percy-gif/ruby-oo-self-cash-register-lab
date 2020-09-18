b7930b83eb0d78078b223d22f2da1de0fa35ebce
Percy-gif <percymajor@gmail.com>
2020-09-18 01:26:41 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,34 @@
+class CashRegister 
+     attr_accessor :total  
+     attr_reader :discount
 
+    def initialize(total = 0, discount = 20)
+        @total = total 
+        @discount = discount
+        
+    end
+
+    def total
+        @total
+    end 
+    
+ 
+
+    def add_item(title, price)
+        self.total += 0.98
+        
+    end 
+
+    def apply_discount 
+        @discount
+    end 
+
+    # def items 
+    # end 
+
+    # def void_last_transaction
+    #
+    # end 
+
+
+end
